1d28a317acc4d54f0096ff85528749891b7137f2
Yvette Rimas <daniellayvetterimas@gmail.com>
2026-06-25 17:16:15 +0800

    Add project starter

---
+++ MiniEmployeeDirectory/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiniEmployeeDirectory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
