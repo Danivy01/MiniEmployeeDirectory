1441803c3b9ce8b83d8d671ccf3d0b3d4baf25a2
Yvette Rimas <daniellayvetterimas@gmail.com>
2026-06-26 16:43:22 +0800

    Add IEmployeeService and inject it into HomeController

--- MiniEmployeeDirectory/Controllers/HomeController.cs
+++ MiniEmployeeDirectory/Controllers/HomeController.cs
@@ -1,21 +1,31 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using MiniEmployeeDirectory.Interface;
 using MiniEmployeeDirectory.Models;
+using MiniEmployeeDirectory.Service;
 
 namespace MiniEmployeeDirectory.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IEmployeeService employee;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IEmployeeService employee)
         {
             _logger = logger;
+            this.employee = employee;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var employees = employee.GetEmployees();
+            string result = "";
+            
+            foreach (var emp in employees) {
+                result += $"ID: {emp.EmployeeID} Full name: {emp.FullName} Department: {emp.Department} Position: {emp.Position}\n";
+            }
+            return Content(result);
         }
 
         public IActionResult Privacy()
