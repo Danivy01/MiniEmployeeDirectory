6506f7a4a62799cb70e9e6a20867a39777c828b5
Yvette Rimas <daniellayvetterimas@gmail.com>
2026-06-26 16:04:41 +0800

    Add Employee Service

---
+++ MiniEmployeeDirectory/Service/EmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using MiniEmployeeDirectory.Interface;
+using MiniEmployeeDirectory.Models;
+
+namespace MiniEmployeeDirectory.Service {
+    public class EmployeeService : IEmployeeService {
+        public List<Employee> GetEmployees() {
+            return new List<Employee> {
+                new Employee { ID = 1, EmployeeID = "MA789456", FullName = "Janna Rose A. Chavez", Department = "Accounting Dept.", Position = "Accountant" },
+                new Employee { ID = 1, EmployeeID = "MA951852", FullName = "Adelene Kate C. Pilar", Department = "HR Dept.", Position = "Hiring Manager" }
+            };
+        }
+    }
+}
