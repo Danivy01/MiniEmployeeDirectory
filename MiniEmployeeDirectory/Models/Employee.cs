55eb1a761e959a9435d431df47ce55a087368d41
Yvette Rimas <daniellayvetterimas@gmail.com>
2026-06-26 15:47:27 +0800

    Add Employee Model

---
+++ MiniEmployeeDirectory/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniEmployeeDirectory.Models {
+    public class Employee {
+        public int ID { get; set; }
+        public string EmployeeID { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+    }
+}
